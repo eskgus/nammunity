860efebac3bfa68dd716d0804143fa0cf1c3e631
eskgus <eskgus012@naver.com>
2023-03-26 02:38:33 +0900

    게시글 읽기 + 조회수 테스트

--- src/test/java/com/eskgus/nammunity/web/IndexControllerTest.java
+++ src/test/java/com/eskgus/nammunity/web/IndexControllerTest.java
@@ -1,5 +1,7 @@
 package com.eskgus.nammunity.web;
 
+import com.eskgus.nammunity.domain.posts.Posts;
+import com.eskgus.nammunity.domain.posts.PostsRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,10 +18,26 @@ public class IndexControllerTest {
     @Autowired
     private TestRestTemplate testRestTemplate;
 
+    @Autowired
+    private PostsRepository postsRepository;
+
     @Test
     public void loadMainPage() {
         String body = this.testRestTemplate.getForObject("/", String.class);
 
         Assertions.assertThat(body).contains("나뮤니티: 나현이가 만든 커뮤니티");
+    }
+
+    @Test
+    public void readAPost() {
+        postsRepository.save(Posts.builder().title("title").content("content").author("author").build());
+
+        Posts posts = postsRepository.findAll().get(0);
+
+        String body = this.testRestTemplate.getForObject("/posts/read/" + posts.getId(), String.class);
+        Assertions.assertThat(body).contains("value=\"title\"");
+
+        posts = postsRepository.findAll().get(0);
+        Assertions.assertThat(posts.getView()).isEqualTo(1);
     }
 }
