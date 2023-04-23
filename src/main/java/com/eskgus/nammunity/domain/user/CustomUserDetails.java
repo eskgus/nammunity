4e8157e919e4411f1669b917abcf0304c07ecb4f
eskgus <eskgus012@naver.com>
2023-04-24 00:12:42 +0900

    로그인 화면 + 닉네임 표시

--- src/main/java/com/eskgus/nammunity/domain/user/CustomUserDetails.java
+++ src/main/java/com/eskgus/nammunity/domain/user/CustomUserDetails.java
@@ -28,6 +28,10 @@ public class CustomUserDetails implements UserDetails {
         return user.getUsername();
     }
 
+    public String getNickname() {
+        return user.getNickname();
+    }
+
     @Override
     public boolean isAccountNonExpired() {
         return true;
