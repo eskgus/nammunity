87602dcd1c2b095047b8c1c9bf598546a556e384
eskgus <eskgus012@naver.com>
2023-05-09 22:42:25 +0900

    취소 버튼 url

--- src/main/java/com/eskgus/nammunity/handler/TemplateAdvice.java
+++ src/main/java/com/eskgus/nammunity/handler/TemplateAdvice.java
@@ -1,17 +1,33 @@
 package com.eskgus.nammunity.handler;
 
 import com.eskgus.nammunity.domain.user.CustomUserDetails;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @ControllerAdvice
 public class TemplateAdvice {
     @ModelAttribute
-    public void addDefaultAttributes(@AuthenticationPrincipal CustomUserDetails user, Model model) {
+    public void addDefaultAttributes(@AuthenticationPrincipal CustomUserDetails user, Model model,
+                                     HttpServletRequest request) {
+        Map<String, String> attr = new HashMap<>();
+
         if (user != null) {
-            model.addAttribute("auth", user.getNickname());
+            attr.put("auth", user.getNickname());
         }
+
+        Object url = request.getSession().getAttribute("prePage");
+        if (url != null) {
+            attr.put("prePage", url.toString());
+        } else {
+            attr.put("prePage", "/");
+        }
+
+        model.addAllAttributes(attr);
     }
 }
