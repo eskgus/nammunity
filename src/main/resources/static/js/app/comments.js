3e7c8960b1bf6fb938c0923dde6f1fec005b8d30
eskgus <eskgus012@naver.com>
2023-08-25 19:00:00 +0900

    comments/likes.js에 객체 리터럴 패턴 적용

--- src/main/resources/static/js/app/comments.js
+++ src/main/resources/static/js/app/comments.js
@@ -1,145 +1,154 @@
-document.getElementById('btn-cmt-save').addEventListener('click', function() {
-    saveComments();
-});
+var commentsMain = {
+    // event 함수에서 쓸 변수 초기화
+    e: false,
+    di: null,
+    re: null,
+    rdi: null,
 
-var e = false;
-var di;
-var [re, rdi] = [,];
-event('edit', edit);
-event('delete', deleteComments);
+    init: function() {
+        var _this = this;
 
-function saveComments() {
-    var data = {
-        content: $('#cmt-content').val(),
-        postsId: $('#id').val()
-    };
-
-    $.ajax({
-        type: 'POST',
-        url: '/api/comments',
-        dataType: 'json',
-        contentType: 'application/json; charset=utf-8',
-        data: JSON.stringify(data)
-    }).done(function(response) {
-        if (Object.keys(response) == 'OK') {
-            window.location.reload();
-        } else {
-            alert(response[Object.keys(response)]);
-            if (Object.keys(response) == 'error') {
-                window.location.href = '/';
-            }
-        }
-    }).fail(function(response) {
-        alert(JSON.stringify(response));
-    });
-}
-
-function event(name, func) {
-    var btns = document.getElementsByName('btn-cmt-' + name);
-    for (let i = 0; i < btns.length; i++) {
-        btns[i].addEventListener('click', function() {
-            if (e == true && (func.name == 'edit' || func.name.includes('delete'))) {
-                cancel(di);
-            }
-
-            if (func.name == 'edit' || func.name.includes('delete')) {
-                [re, rdi] = func(btns[i]);
-            } else {
-                [re, rdi] = func(di);
-            }
-
-            if (re == 'e') {
-                e = true;
-            } else {
-                e = false;
-            }
-            di = rdi;
+        // 댓글 저장
+        document.getElementById('btn-cmt-save').addEventListener('click', function() {
+            _this.saveComments();
         });
-    }
-}
 
-function edit(btn) {
-    var divId = $(btn)[0].href.slice($(btn)[0].href.indexOf('#') + 1);
-    var div = document.getElementById(divId);
+        // 댓글 수정
+        _this.event('edit', _this.editComments);
 
-    var cmt = div.children[0];
-    var data = {
-        author: cmt.children[0].innerHTML,
-        content: cmt.children[2].innerHTML,
-        date: cmt.children[3].innerHTML,
-        id: divId.slice(divId.indexOf('-') + 1)
-    };
-
-    cmt.style = 'display: none';
-
-    var html = '<div class="comments-creation">'
-                + '<span class="author">' + data.author + '</span>'
-                + '<textarea class="cmt-content" id="cmt-content-' + data.id + '" rows="4">' + data.content + '</textarea>'
-                + '<div class="btn-area">'
-                + '<button type="button" class="btn" name="btn-cmt-update">수정</button>'
-                + '<button type="button" class="btn" name="btn-cmt-cancel">취소</button>'
-                + '</div></div>';
-    div.insertAdjacentHTML('beforeend', html);
-
-    event('update', update);
-    event('cancel', cancel);
-
-    return ['e', divId];
-}
-
-function update(divId) {
-    var id =  divId.slice(divId.indexOf('-') + 1);
-    var data = {
-        content: document.getElementById(divId).children[1].children[1].value
-    };
-
-    $.ajax({
-        type: 'PUT',
-        url: '/api/comments/' + id,
-        contentType: 'application/json; charset=utf-8',
-        data: JSON.stringify(data)
-    }).done(function(response) {
-        if (Object.keys(response) == 'OK') {
-            window.location.reload();
-        } else {
-            alert(response[Object.keys(response)]);
-            if (Object.keys(response) == 'error') {
-                window.location.reload();
-            }
-        }
-    }).fail(function(response) {
-        alert(JSON.stringify(response));
-    });
-
-    return ['u', divId];
-}
-
-function cancel(divId) {
-    var div = document.getElementById(divId);
-
-    div.children[0].style = 'display: block';
-    div.children[1].remove();
-
-    return ['c', null];
-}
-
-function deleteComments(btn) {
-    var divId;
-    if (confirm('정말로 삭제하시겠어요?')) {
-        var href = btn.parentElement.previousElementSibling.children[0].href;
-        divId = href.slice(href.indexOf('#') + 1);
-        var id = divId.slice(divId.indexOf('-') + 1);
+        // 댓글 삭제
+        _this.event('delete', _this.deleteComments);
+    },
+    saveComments: function() {
+        var data = {
+            content: $('#cmt-content').val(),
+            postsId: $('#id').val()
+        };
 
         $.ajax({
-            type: 'DELETE',
-            url: '/api/comments/' + id,
-            contentType: 'application/json; charset=utf-8'
+            type: 'POST',
+            url: '/api/comments',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
         }).done(function(response) {
-            alert(response[Object.keys(response)]);
-            window.location.reload();
+            if (Object.keys(response) == 'OK') {
+                window.location.reload();
+            } else {
+                alert(response[Object.keys(response)]);
+                if (Object.keys(response) == 'error') {
+                    window.location.href = '/';
+                }
+            }
         }).fail(function(response) {
             alert(JSON.stringify(response));
         });
+    },
+    event: function(name, func) {
+        var btns = document.getElementsByName('btn-cmt-' + name);
+        for (let i = 0; i < btns.length; i++) {
+            btns[i].addEventListener('click', function() {
+                if (commentsMain.e == true && (func.name.includes('edit') || func.name.includes('delete'))) {
+                    commentsMain.cancelEditComments(commentsMain.di);
+                }
+
+                if (func.name.includes('edit') || func.name.includes('delete')) {
+                    [commentsMain.re, commentsMain.rdi] = func(btns[i]);
+                } else {
+                    [commentsMain.re, commentsMain.rdi] = func(commentsMain.di);
+                }
+
+                if (commentsMain.re == 'e') {
+                    commentsMain.e = true;
+                } else {
+                    commentsMain.e = false;
+                }
+                commentsMain.di = commentsMain.rdi;
+            });
+        }
+    },
+    editComments: function(btn) {
+        var divId = $(btn)[0].href.slice($(btn)[0].href.indexOf('#') + 1);
+        var div = document.getElementById(divId);
+
+        var cmt = div.children[0];
+        var data = {
+            author: cmt.children[0].innerHTML,
+            content: cmt.children[2].innerHTML,
+            date: cmt.children[3].innerHTML,
+            id: divId.slice(divId.indexOf('-') + 1)
+        };
+
+        cmt.style = 'display: none';
+
+        var html = '<div class="comments-creation">'
+                    + '<span class="author">' + data.author + '</span>'
+                    + '<textarea class="cmt-content" id="cmt-content-' + data.id + '" rows="4">' + data.content + '</textarea>'
+                    + '<div class="btn-area">'
+                    + '<button type="button" class="btn" name="btn-cmt-update">수정</button>'
+                    + '<button type="button" class="btn" name="btn-cmt-cancel">취소</button>'
+                    + '</div></div>';
+        div.insertAdjacentHTML('beforeend', html);
+
+        commentsMain.event('update', commentsMain.updateComments);
+        commentsMain.event('cancel', commentsMain.cancelEditComments);
+
+        return ['e', divId];
+    },
+    updateComments: function(divId) {
+        var id = divId.slice(divId.indexOf('-') + 1);
+        var data = {
+            content: document.getElementById(divId).children[1].children[1].value
+        };
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/comments/' + id,
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function(response) {
+            if (Object.keys(response) == 'OK') {
+                window.location.reload();
+            } else {
+                alert(response[Object.keys(response)]);
+                if (Object.keys(response) == 'error') {
+                    window.location.reload();
+                }
+            }
+        }).fail(function(response) {
+            alert(JSON.stringify(response));
+        });
+
+        return ['u', divId];
+    },
+    cancelEditComments: function(divId) {
+        var div = document.getElementById(divId);
+
+        div.children[0].style = 'display: block';
+        div.children[1].remove();
+
+        return ['c', null];
+    },
+    deleteComments: function(btn) {
+        var divId;
+        if (confirm('정말로 삭제하시겠어요?')) {
+            var href = btn.parentElement.previousElementSibling.children[0].href;
+            divId = href.slice(href.indexOf('#') + 1);
+            var id = divId.slice(divId.indexOf('-') + 1);
+
+            $.ajax({
+                type: 'DELETE',
+                url: '/api/comments/' + id,
+                contentType: 'application/json; charset=utf-8'
+            }).done(function(response) {
+                alert(response[Object.keys(response)]);
+                window.location.reload();
+            }).fail(function(response) {
+                alert(JSON.stringify(response));
+            });
+        }
+        return ['d', divId];
     }
-    return ['d', divId];
-}
+};
+
+commentsMain.init();
--- src/main/resources/static/js/app/likes.js
+++ src/main/resources/static/js/app/likes.js
@@ -1,108 +1,114 @@
-var pLikeBtn =  document.getElementById('btn-posts-like');
-pLikeBtn.addEventListener('click', function() {
-    var post = {
-        id: $('#id').val(),
-        img: pLikeBtn.children[0].classList,
-        sum: pLikeBtn.nextElementSibling
-    };
+var likesMain = {
+    init: function() {
+        var _this = this;
 
-    if (post.img[1] == 'like_s_d') {
-        deletePostLikes(post);
-    } else {
-        savePostLikes(post);
+        // 게시글 좋아요
+        var pLikeBtn = document.getElementById('btn-posts-like');
+        pLikeBtn.addEventListener('click', function() {
+            var post = {
+                id: $('#id').val(),
+                img: pLikeBtn.children[0].classList,
+                sum: pLikeBtn.nextElementSibling
+            };
+
+            if (post.img[1] == 'like_s_d') {
+                _this.deletePostLikes(post);
+            } else {
+                _this.savePostLikes(post);
+            }
+        });
+
+        // 댓글 좋아요
+        var cLikeBtns = document.getElementsByName('btn-cmts-like');
+        for (let i = 0; i < cLikeBtns.length; i++) {
+            cLikeBtns[i].addEventListener('click', function() {
+                var divId = cLikeBtns[i].closest('.comment').parentElement.id;
+                var cmt = {
+                    id: divId.slice(divId.indexOf('-') + 1),
+                    img: cLikeBtns[i].children[0].classList,
+                    sum: cLikeBtns[i].nextElementSibling
+                };
+
+                if (cmt.img[1] == 'like_s_d') {
+                    _this.deleteCmtLikes(cmt);
+                } else {
+                    _this.saveCmtLikes(cmt);
+                }
+            });
+        }
+    },
+    savePostLikes: function(post) {
+        $.ajax({
+            type: 'POST',
+            url: '/api/likes?postsId=' + post.id
+        }).done(function(response) {
+            if (Object.keys(response) == 'OK') {
+                post.img.add('like_s_d');
+                post.sum.textContent = parseInt(post.sum.textContent) + 1;
+            } else {
+                alert(response[Object.keys(response)]);
+                window.location.href = '/';
+            }
+        }).fail(function(response) {
+            if (response.status == 401) {
+                alert('로그인하세요.');
+            } else {
+                alert(JSON.stringify(response));
+            }
+        });
+    },
+    deletePostLikes: function(post) {
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/likes?postsId=' + post.id
+        }).done(function(response) {
+            if (response == 'OK') {
+                post.img.remove('like_s_d');
+                post.sum.textContent = parseInt(post.sum.textContent) - 1;
+            } else {
+                alert(response);
+                window.location.href = '/';
+            }
+        }).fail(function(response) {
+            alert(JSON.stringify(response));
+        });
+    },
+    saveCmtLikes: function(cmt) {
+        $.ajax({
+            type: 'POST',
+            url: '/api/likes?commentsId=' + cmt.id
+        }).done(function(response) {
+            if (Object.keys(response) == 'OK') {
+                cmt.img.add('like_s_d');
+                cmt.sum.textContent = parseInt(cmt.sum.textContent) + 1;
+            } else {
+                alert(response[Object.keys(response)]);
+                window.location.href = '/';
+            }
+        }).fail(function(response) {
+            if (response.status == 401) {
+                alert('로그인하세요.');
+            } else {
+                alert(JSON.stringify(response));
+            }
+        });
+    },
+    deleteCmtLikes: function(cmt) {
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/likes?commentsId=' + cmt.id
+        }).done(function(response) {
+            if (response == 'OK') {
+                cmt.img.remove('like_s_d');
+                cmt.sum.textContent = parseInt(cmt.sum.textContent) - 1;
+            } else {
+                alert(response);
+                window.location.href = '/';
+            }
+        }).fail(function(response) {
+            alert(JSON.stringify(response));
+        });
     }
-});
+};
 
-var cLikeBtns = document.getElementsByName('btn-cmts-like');
-for (let i = 0; i < cLikeBtns.length; i++) {
-    cLikeBtns[i].addEventListener('click', function() {
-        var divId = cLikeBtns[i].closest('.comment').parentElement.id;
-        var cmt = {
-            id: divId.slice(divId.indexOf('-') + 1),
-            img: cLikeBtns[i].children[0].classList,
-            sum: cLikeBtns[i].nextElementSibling
-        };
-
-        if (cmt.img[1] == 'like_s_d') {
-            deleteCmtLikes(cmt);
-        } else {
-            saveCmtLikes(cmt);
-        }
-    });
-}
-
-function savePostLikes(post) {
-    $.ajax({
-        type: 'POST',
-        url: '/api/likes?postsId=' + post.id
-    }).done(function(response) {
-        if (Object.keys(response) == 'OK') {
-            post.img.add('like_s_d');
-            post.sum.textContent = parseInt(post.sum.textContent) + 1;
-        } else {
-            alert(response[Object.keys(response)]);
-            window.location.href = '/';
-        }
-    }).fail(function(response) {
-        if (response.status == 401) {
-            alert('로그인하세요.');
-        } else {
-            alert(JSON.stringify(response));
-        }
-    });
-}
-
-function deletePostLikes(post) {
-    $.ajax({
-        type: 'DELETE',
-        url: '/api/likes?postsId=' + post.id
-    }).done(function(response) {
-        if (response == 'OK') {
-            post.img.remove('like_s_d');
-            post.sum.textContent = parseInt(post.sum.textContent) - 1;
-        } else {
-            alert(response);
-            window.location.href = '/';
-        }
-    }).fail(function(response) {
-        alert(JSON.stringify(response));
-    });
-}
-
-function saveCmtLikes(cmt) {
-    $.ajax({
-        type: 'POST',
-        url: '/api/likes?commentsId=' + cmt.id
-    }).done(function(response) {
-        if (Object.keys(response) == 'OK') {
-            cmt.img.add('like_s_d');
-            cmt.sum.textContent = parseInt(cmt.sum.textContent) + 1;
-        } else {
-            alert(response[Object.keys(response)]);
-            window.location.href = '/';
-        }
-    }).fail(function(response) {
-        if (response.status == 401) {
-            alert('로그인하세요.');
-        } else {
-            alert(JSON.stringify(response));
-        }
-    });
-}
-
-function deleteCmtLikes(cmt) {
-    $.ajax({
-        type: 'DELETE',
-        url: '/api/likes?commentsId=' + cmt.id
-    }).done(function(response) {
-        if (response == 'OK') {
-            cmt.img.remove('like_s_d');
-            cmt.sum.textContent = parseInt(cmt.sum.textContent) - 1;
-        } else {
-            alert(response);
-            window.location.href = '/';
-        }
-    }).fail(function(response) {
-        alert(JSON.stringify(response));
-    });
-}
+likesMain.init();
